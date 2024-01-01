7f72caa6e52c42d042d7d8a6cd109b5acbef6daa
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2024-01-01 17:30:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
+var img = "" ;
+var status = "" ;
+
 function preload() {
     img = loadImage("dog_cat.jpg") ;
 }
 
 function setup() {
     canvas = createCanvas(640 , 420) ;
-    canvas.center() ;
+    canvas.position(460 , 150) ;
+    objectDetector = ml5.objectDetector("cocossd" , modelLoaded) ;
+    document.getElementById("status").innerHTML = "Status : Detecting Objects" ;
+}
+
+function modelLoaded() {
+    console.log("Modal is Initialised !") ;
+    status = true ;
+    objectDetector.detect(img , gotResult) ;
+}
+
+function  gotResult(error , results) {
+    if(error) {
+        console.error(error) ;
+    } 
+        console.log(results) ;
 }
 
 function draw() {
@@ -14,4 +32,10 @@ function draw() {
     text("Dog" , 45 , 75) ;
     noFill() ;
     rect(30 , 60 , 450 , 350) ;
+
+    fill('#FF0000') ;
+    stroke('#FF0000') ;
+    text("Cat" , 320 , 120) ;
+    noFill() ;
+    rect(300 , 90 , 270 , 320) ;
 }
