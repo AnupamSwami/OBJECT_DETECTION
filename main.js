36c7cc62dc641f5579ad28346b2bfc33037ad585
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2024-01-10 18:42:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ img = "" ;
 status = "" ;
 objects = {} ;
 
-function preload() {
-    img = loadImage("dog_cat.jpg") ;
-}
+function preload() {}
 
 function setup() {
-    canvas = createCanvas(640 , 420) ;
+    canvas = createCanvas(380 , 380) ;
     canvas.position(460 , 150) ;
+    video = createCapture(VIDEO) ;
+    video.size(380 , 380) ;
+    video.hide() ;
+}
+
+function start() {
     objectDetector = ml5.objectDetector("cocossd" , modelLoaded) ;
     document.getElementById("status").innerHTML = "Status : Detecting Objects" ;
 }
@@ -16,7 +20,6 @@ function setup() {
 function modelLoaded() {
     console.log("CoCoSSD is Initialised !") ;
     status = true ;
-    objectDetector.detect(img , gotResult) ;
 }
 
 function  gotResult(error , results) {
@@ -28,18 +31,22 @@ function  gotResult(error , results) {
 }
 
 function draw() {
-    image(img , 0 , 0 , 640 , 420) ;
+    image(video , 0 , 0 , 380 , 380) ;
 
     if(status != "") {
-    }
-
-    for(i = 0 ; i < objects.length ; i++) {
-        document.getElementById("status").innerHTML = "Status : Objects Detected" ;
-        fill('#FF0000') ;
-        stroke('#FF0000') ;
-        noFill() ;
-        percent = floor(objects[i].confidence * 100) ;
-        text(objects[i].label + "" + percent + "%" , objects[i].x , objects[i].y) ;
-        rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height) ;
+        objectDetector.detect(video , gotResult) ;
+        r = random(255) ;
+        g = random(255) ;
+        b = random(255) ;
+        for(i = 0 ; i < objects.length ; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected" ;
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects that Detected are : " + objects.length ;
+            fill(r , g , b) ; ;
+            stroke(r , g , b) ;
+            noFill() ;
+            percent = floor(objects[i].confidence * 100) ;
+            text(objects[i].label + "" + percent + "%" , objects[i].x , objects[i].y) ;
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height) ;
+        }
     }
 }
